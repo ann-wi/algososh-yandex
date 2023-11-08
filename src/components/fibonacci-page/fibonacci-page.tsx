59cbe6fa4ff82d61e0859ac947c877bdb976f8d9
Anya <vann2299@yandex.ru>
2023-11-08 09:31:29 +0300

    functionality for String reverse done, Fibonacci almost done

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,68 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
+import FibonacciPageStyles from "./fibonacci-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
 
 export const FibonacciPage: React.FC = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [fibResult, setFibResult] = useState<string[]>([]);
+
+  // make it accept only numbers not bigger than 19
+
+  const handleChange = (e: FormEvent<HTMLInputElement>): void => {
+    setInputValue(e.currentTarget.value);
+  };
+
+  const calcFibonacci = (number: string) => {
+    const toNumber = Number(number);
+    const defArray = ["0", "1"];
+
+    for (let i = 2; i < toNumber; i++) {
+      const res = Number(defArray[i - 1]) + Number(defArray[i - 2]);
+      defArray.push(String(res));
+    }
+
+    return defArray;
+  };
+
+  const getFibonacci = async () => {
+    let result: string[] = calcFibonacci(inputValue);
+
+    for (let i = 0; i <= result.length; i++) {
+      // await
+      setFibResult(result.slice(0, i + 1));
+    }
+  };
+
+  const startFibonacci = (
+    e: FormEvent<HTMLFormElement> | FormEvent<HTMLButtonElement>
+  ): void => {
+    e.preventDefault();
+    getFibonacci();
+    console.log(fibResult);
+  };
+
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+      <form className={FibonacciPageStyles.form}>
+        <div className={FibonacciPageStyles.container}>
+          <Input
+            placeholder={"Введите число"}
+            isLimitText={true}
+            maxLength={2}
+            type={"num"}
+            onChange={handleChange}
+            value={inputValue}
+            max={19}
+          />
+          <Button
+            text={"Рассчитать"}
+            extraClass={"button-style"}
+            onClick={startFibonacci}
+          />
+        </div>
+      </form>
     </SolutionLayout>
   );
 };
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import StringPageStyles from "./string.module.css";
 import { Button } from "../ui/button/button";
 import { Input } from "../ui/input/input";
@@ -7,6 +7,50 @@ import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
 export const StringComponent: React.FC = () => {
   const [inputString, setInputString] = useState<string>("");
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [reversedArr, setReversedArr] = useState<string[]>([]);
+
+  const handleChange = (e: FormEvent<HTMLInputElement>): void => {
+    setInputString(e.currentTarget.value.trim());
+  };
+
+  const swapString = (
+    arr: string[],
+    firstIndex: number,
+    secondIndex: number
+  ): void => {
+    [arr[firstIndex], arr[secondIndex - firstIndex]] = [
+      arr[secondIndex - firstIndex],
+      arr[firstIndex],
+    ];
+  };
+
+  const reverseString = async (item: string) => {
+    // delete spaces from a string
+    const modifiedString = item.split("");
+
+    //
+    setCurrentIndex(0);
+    setReversedArr([...modifiedString]);
+
+    // await delay()
+    for (let i = 0; i < Math.floor(modifiedString.length / 2); i++) {
+      swapString(modifiedString, i, modifiedString.length - 1);
+      setCurrentIndex((i: number) => i + 1);
+      setReversedArr([...modifiedString]);
+      //await delay
+    }
+    setCurrentIndex((i: number) => i + 1);
+    return modifiedString;
+  };
+
+  const startReversing = (
+    e: FormEvent<HTMLFormElement> | FormEvent<HTMLButtonElement>
+  ): void => {
+    e.preventDefault();
+    reverseString(inputString);
+    setInputString("");
+  };
 
   return (
     <SolutionLayout title="Строка">
@@ -15,13 +59,24 @@ export const StringComponent: React.FC = () => {
         onSubmit={() => console.log("string sort")}
       >
         <div className={StringPageStyles.container}>
-          <Input placeholder="Введите текст" value={inputString || ""} />
-          <Button />
+          <Input
+            placeholder="Введите текст"
+            value={inputString || ""}
+            onChange={handleChange}
+            isLimitText={true}
+            maxLength={11}
+          />
+          <Button
+            text="Развернуть"
+            onClick={startReversing}
+            disabled={!inputString}
+          />
         </div>
         <ul className={StringPageStyles.circleList}>
-          <li className={StringPageStyles.circle}>
-            <Circle />
-          </li>
+          {reversedArr &&
+            reversedArr?.map((item, index: number) => {
+              console.log(item);
+            })}
         </ul>
       </form>
     </SolutionLayout>
